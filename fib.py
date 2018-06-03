fef3f924e756113bdd14e6474be6bacb31b7be25
benbprograms <33466059+benbprograms@users.noreply.github.com>
2018-06-03 19:01:26 +0100

    added Main code

---
+++ fib.py
@@ -0,0 +1,13 @@
+def fib(n):
+    a, b = 0, 1
+    while a < n:
+        print(a, end='\n')
+        a, b = b, a + b
+    print("\n")
+
+
+many = input("how far do you want the fibonacci sequence up to: ")
+try:
+    fib(int(many))
+except ValueError:
+    where = input("enter a number only: ")
